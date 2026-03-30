27a9117b31d29a6b51be6c06452cc958a91cf016
Bhargav-Dodda-07 <bhargavdodda969@gmail.com>
2026-03-30 17:34:42 +0530

    expense-app-dev-backend-alb

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -16,3 +16,20 @@ resource "aws_lb" "backend-alb" {
     }
   )
 }
+
+# Backend ALB listening on port no 80
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.backend_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Hi, I am from Backend ALB HTTP"
+      status_code  = "200"
+    }
+  }
+}
