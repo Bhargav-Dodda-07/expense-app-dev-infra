19dee565ff693134e3aafddefffab0ef9619428d
Bhargav-Dodda-07 <bhargavdodda969@gmail.com>
2026-03-30 17:16:09 +0530

    expense-app-dev-backend-alb

---
+++ 50-backend-alb/main.tf
@@ -0,0 +1,18 @@
+resource "aws_lb" "backend-alb" {
+  name               = "${local.common_name_suffix}-backend-alb"
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [local.backend_alb_sg_id]
+
+  # it should be created under private subnet
+  subnets            = [local.public_subnet_ids]
+
+  enable_deletion_protection = true
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-backend-alb"
+    }
+  )
+}
